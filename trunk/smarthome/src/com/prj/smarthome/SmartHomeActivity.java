4d20f334e58360ab8bee889774253b183103e6b3
zengtao.routon@gmail.com <zengtao.routon@gmail.com@21f58cad-dc82-5013-228e-78700e9fa569>
2012-07-23 03:56:38 +0000

    a little change for test commit

--- trunk/smarthome/src/com/prj/smarthome/SmartHomeActivity.java
+++ trunk/smarthome/src/com/prj/smarthome/SmartHomeActivity.java
@@ -5,6 +5,7 @@ import android.os.Bundle;
 
 public class SmartHomeActivity extends Activity {
     /** Called when the activity is first created. */
+	/** yes , I change a little and commit to GC **/
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
