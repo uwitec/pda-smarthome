4ed422e3cd6d58394c5272aee55ce03d40dab91c
zengtao.routon <zengtao.routon@21f58cad-dc82-5013-228e-78700e9fa569>
2012-09-03 10:42:14 +0000

    基本功能完成

--- trunk/smarthome/src/com/prj/smarthome/MultiCameraActivity.java
+++ trunk/smarthome/src/com/prj/smarthome/MultiCameraActivity.java
@@ -17,9 +17,10 @@ import android.widget.VideoView;
 import com.prj.smarthome.OneCamera;
 
 /*
- * 该播放器不支持rtsp需要用户授权的视频流
+ * 该播放器不支持需要用户授权的rtsp视频流
  */
-public class MultiCameraActivity extends ActivityGroup {	
+public class MultiCameraActivity extends ActivityGroup {
+	
 	static int onclickettime = 0;
 	
 	@Override
@@ -27,7 +28,6 @@ public class MultiCameraActivity extends ActivityGroup {
 		// TODO Auto-generated method stub
 		super.onDestroy();
 		onclickettime = 0;
-		Log.i("ZT", "onclickettime=0");
 	}
 	
     public void onCreate(Bundle savedInstanceState) {
@@ -36,12 +36,7 @@ public class MultiCameraActivity extends ActivityGroup {
         
         Button addaview = (Button) findViewById(R.id.playerbutton);
         addaview.setOnClickListener(new View.OnClickListener(){
-			public void onClick(View v){
-				String ip = InitRtspIP();
-				String video_uri = "rtsp://" + ip + ":554/H264";
-				OneCamera.video_uri = video_uri;
-				
-				Log.i("ZT", video_uri);
+			public void onClick(View v){			
 	        	Intent intent = new Intent(MultiCameraActivity.this, OneCamera.class);	
 	        	add_activitygroup(MultiCameraActivity.this, R.id.CameraslinearLayout, intent);
 	        }
@@ -49,33 +44,79 @@ public class MultiCameraActivity extends ActivityGroup {
 	}
 	
 	@SuppressLint("NewApi")
-	String InitRtspIP()
+	String getCameraIP()
 	{
 		EditText ipin = (EditText) findViewById(R.id.camera_ip);    	
 		String ip = ipin.getText().toString();
 		if(ip.isEmpty())
 			/* rtsp H264 stream (default)*/
-			ip = "admin:nimda@172.16.55.47";
+			ip = "172.16.55.47";
+
+		/*	成都交通摄像头 ok */ 
+//			video_uri = "rtsp://218.205.231.149:554/live/1/35AB46A109903442/0037f42790b7cb14.sdp?id=guest&t=1305311828&en=a4d5d7adaf0eca0d907875009a901a85&rs=wap";
+
+    	return ip;
+    }
+
+	String getRtspPort()
+	{
+		EditText ipin = (EditText) findViewById(R.id.camera_rtsp_port);    	
+		String pt = ipin.getText().toString();
+		if(pt.isEmpty())
+			/* rtsp H264 stream (default)*/
+			pt = "554";
+    	return pt;
+    }
 	
-			/*	成都交通摄像头 ok */ 
-	//		video_uri = "rtsp://218.205.231.149:554/live/1/35AB46A109903442/0037f42790b7cb14.sdp?id=guest&t=1305311828&en=a4d5d7adaf0eca0d907875009a901a85&rs=wap";
-	 	    	
-	    	return ip;
-	    }
-	
-		void add_activitygroup(ActivityGroup actgp, int resid, Intent intent) {
-			onclickettime++;
-			if(onclickettime>=5)
-			{
-				String st = "最多创建4个视频，性能考虑";
-				Toast.makeText(MultiCameraActivity.this, st, Toast.LENGTH_SHORT).show();
-				return ;
-			}
-			String unq = String.format("AB%d", onclickettime);	
-			
-			LinearLayout layout = (LinearLayout)actgp.findViewById(resid);
-			Window subActivity = actgp.getLocalActivityManager().startActivity(unq, intent);				
-			View view = subActivity.getDecorView();  
-		    layout.addView(view);
+	String getPtzPort()
+	{
+		EditText ipin = (EditText) findViewById(R.id.camera_ptz_port);    	
+		String pt = ipin.getText().toString();
+		if(pt.isEmpty())
+			/* rtsp H264 stream (default)*/
+			pt = "81";
+    	return pt;
+    }
+
+	String getLoginUser()
+	{
+		EditText ipin = (EditText) findViewById(R.id.camera_login_user);    	
+		String pt = ipin.getText().toString();
+		if(pt.isEmpty())
+			/* rtsp H264 stream (default)*/
+			pt = "admin";
+    	return pt;
+    }
+
+	String getLoginPwd()
+	{
+		EditText ipin = (EditText) findViewById(R.id.camera_login_pwd);    	
+		String pt = ipin.getText().toString();
+		if(pt.isEmpty())
+			/* rtsp H264 stream (default)*/
+			pt = "nimda";
+    	return pt;
+    }
+
+	void add_activitygroup(ActivityGroup actgp, int resid, Intent intent) {
+		onclickettime++;
+		if(onclickettime>=5)
+		{
+			String st = "最多创建4个视频，性能考虑";
+			Toast.makeText(MultiCameraActivity.this, st, Toast.LENGTH_SHORT).show();
+			return ;
 		}
+		String unq = String.format("AB%d", onclickettime);	
+		
+		LinearLayout layout = (LinearLayout)actgp.findViewById(resid);
+		Window subActivity = actgp.getLocalActivityManager().startActivity(unq, intent);				
+		View view = subActivity.getDecorView();  
+	    layout.addView(view);
+
+	    OneCamera one = (OneCamera)actgp.getLocalActivityManager().getActivity(unq);
+		one.setPtzCameraIpPort(getCameraIP(), getRtspPort(), getPtzPort());		
+		one.setPtzLoginInfo(getLoginUser(), getLoginPwd());
+		
+	    one.showVideo();
+	}
 }
